7c60a78c0dfa98cdeeee84e44ef536d34bc101df
Irvan Ariyanto <irvan.ariyanto104@gmail.com>
2021-05-01 11:01:17 +0700

    Get Data from API

---
+++ src/components/Posts.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import Post from './Post';
+import axios from 'axios';
+
+class Posts extends Component {
+    state = {
+        posts: []
+    }
+    
+    getData = () => {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(result => {
+            this.setState({
+                posts: result.data
+            });
+        }).catch(err => {
+            throw err;
+        });
+    }
+
+    componentDidMount() {
+        this.getData()
+    }
+    
+    render() {
+        return (
+            this.state.posts.map(post => {
+                return <Post key={post.id} title={post.title}
+                body={post.body}
+                />
+            })
+        );
+    }
+}
+
+export default Posts;
