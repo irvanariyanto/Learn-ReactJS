d4bffa428861031e60699d9a33cb6def225884f1
Irvan Ariyanto <irvan.ariyanto104@gmail.com>
2021-05-01 10:33:12 +0700

    Handling event on Select and Radio

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -5,6 +5,8 @@ class FormInput extends Component {
         nama: '',
         alamat: '',
         hobi: '',
+        mobil: '',
+        jenis_kelamin: 'Laki laki',
     }
 
     handleChange = (e) => {
@@ -28,6 +30,18 @@ class FormInput extends Component {
                     <p>Hobi : {this.state.hobi}</p>
                     <input type="text" name="hobi" value={this.state.hobi} onChange={this.handleChange} />
                 </div>
+                <p>Mobil saya : {this.state.mobil}</p>
+                <select name="mobil" id="" value={this.state.mobil} onChange={this.handleChange}>
+                    <option value="">-</option>
+                    <option value="Fortuner">Fortuner</option>
+                    <option value="Pajero">Pajero</option>
+                    <option value="BMW">BMW</option>
+                    <option value="Merchedes">Merchedes</option>
+                </select>
+
+                <p>Jenis kelamin : {this.state.jenis_kelamin}</p>
+                <input type="radio" name="jenis_kelamin" value="Laki laki" onChange={this.handleChange} checked={this.state.jenis_kelamin == "Laki laki" ? true : false}/>
+                <input type="radio" name="jenis_kelamin" value="Perempuan" onChange={this.handleChange} checked={this.state.jenis_kelamin == "Perempuan" ? true : false}/>
             </div>
         );
     }
